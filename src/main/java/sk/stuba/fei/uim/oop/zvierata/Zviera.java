505ec368ccdb58ae78e878642acaa150477ec139
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-03-09 16:16:11 +0100

    abstraktna trieda

---
+++ src/main/java/sk/stuba/fei/uim/oop/zvierata/Zviera.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop.zvierata;
+
+public abstract class Zviera {
+    protected String meno;
+
+    public abstract void zvukZvierata();
+
+    public void pohladkat(){
+        System.out.println(this.meno + " bol pohladkany");
+        this.zvukZvierata();
+    }
+
+}
