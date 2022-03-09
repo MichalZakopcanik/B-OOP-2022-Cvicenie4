ac0155c136a302b04a61d83561fa5169e87dd082
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-03-09 16:31:01 +0100

    Interface

--- src/main/java/sk/stuba/fei/uim/oop/zvierata/Macka.java
+++ src/main/java/sk/stuba/fei/uim/oop/zvierata/Macka.java
@@ -1,6 +1,6 @@
 package sk.stuba.fei.uim.oop.zvierata;
 
-public class Macka extends Zviera {
+public class Macka extends Zviera implements Behanie {
 
     public Macka(String meno) {
         this.meno = meno;
@@ -10,4 +10,9 @@ public class Macka extends Zviera {
     public void zvukZvierata(){
         System.out.println("mnau mnau");
     };
+
+    @Override
+    public void behaj() {
+        System.out.println("...");
+    }
 }
